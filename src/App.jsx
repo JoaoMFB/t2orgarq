a566d2103b6c71328ae1fd8f38e289fb2d0a80e9
João Marcelo Battaglini <jfbattaglini@gmail.com>
2024-08-21 14:51:25 -0300

    changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PipelineSection from './components/PipelineSection.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <PipelineSection />
+    </div>
+  );
+}
+
+export default App;
